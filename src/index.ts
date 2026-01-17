520eaf021247e7b41f4d5d0781cc241bc19dbc29
ANDEV09 <lamhoangan612@gmail.com>
2026-01-18 03:01:44 +0700

    feat: add CORS configuration for client communication

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
 import userRouter from "./routes/user.routes";
+import cors from "cors";
 
 import "dotenv/config"; // thêm cái này để có thể sử dụng biến môi trường (nếu k sẽ là underfined), import ở file này thì tất cả file con đều được áp dụng
 import { defaultErrorHandler } from "./middlewares/error.middlewares";
@@ -12,6 +13,16 @@ import gameGroupRouter from "~/routes/game-group.routes";
 import gameAccountRouter from "~/routes/game-account.routes";
 const app = express();
 const PORT = process.env.PORT || 8000;
+const allowedOrigins = ["http://localhost:3000"];
+
+const corsOptions = {
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+    credentials: true, // Allow cookies/authorization headers to be sent cross-origin
+};
+
+app.use(cors(corsOptions));
 // parse body request
 app.use(express.json());
 app.use(limiterMiddleware);
